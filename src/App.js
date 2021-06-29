26cba57c32836272e63e623ed44200112011c9bd
haitekie <haimanot.tekie@finn.no>
2021-06-29 11:40:19 +0200

    use styled component - 1st component Nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-
 import { render } from "react-dom";
-
+import Nav from "./common/Nav";
 const App = () => {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+  return <Nav>Hello</Nav>;
 };
 
 //export default App;
 render(<App />, document.getElementById("root"));
+//Nav
+//Title
+//Profile
+// Sign out
+//Profile
+//Avatar
+// Edit
+// Delete
+// Separator
+//TabContainer
+// Tab
+// Tab 2
+// Tab 3
---
+++ src/common/Nav.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+const Nav = styled.div`
+  background-color: green;
+`;
+export default Nav;
