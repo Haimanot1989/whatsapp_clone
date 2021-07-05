ceed5e9efec26dc17b33b9f81803d5fed33ed386
Amanuel Kebedom Tedla <amanuelt@egdeconsulting.no>
2021-07-05 21:59:52 +0200

    Nav meny styling finished

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { FiShare2 } from "react-icons/fi";
 
 const Container = styled.div`
   margin: 30px auto;
@@ -6,12 +7,26 @@ const Container = styled.div`
 
 const List = styled.ul`
   display: grid;
-  grid-template-columns: 80% repeat(2, 1fr);
+  grid-gap: 10px;
   list-style: none;
   background-color: #28957f;
   box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
   color: #fff;
   font-family: sans-serif;
+  height: 55px;
+  align-items: center;
+  .col-1 {
+    grid-column: 1/2;
+  }
+  .col-2 {
+    grid-column: 2/10;
+  }
+  .col-3 {
+    grid-column: 10/11;
+  }
+  .col-4 {
+    grid-column: 11/12;
+  }
 `;
 
 const ListItem = styled.li`
@@ -27,23 +42,26 @@ const Title = styled.span`
 `;
 const navItems = [
   {
-    id: 1,
+    id: 2,
     title: "NextConnect",
   },
   {
-    id: 2,
+    id: 3,
     title: "Profile",
   },
   {
-    id: 3,
+    id: 4,
     title: "Sign out",
   },
 ];
 const Nav = () => (
   <Container>
     <List>
+      <ListItem className="col-1">
+        <FiShare2></FiShare2>
+      </ListItem>
       {navItems.map(({ id, title }) => (
-        <ListItem key={id}>
+        <ListItem key={id} className={`col-${id}`}>
           <Title>{title.toUpperCase()}</Title>
         </ListItem>
       ))}
